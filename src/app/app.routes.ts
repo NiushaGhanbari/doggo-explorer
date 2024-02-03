202dda04acdf48da5ab386fee3c18f75f0be5271
Niusha Ghanbari <niusha.gh93@gmail.com>
2024-02-03 14:09:47 +0100

    Add search results page component and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { SearchResultComponent } from './components/search-result/search-result.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
+    { path: 'result', component: SearchResultComponent },
   ];
   
---
+++ src/app/components/search-result/search-result.component.ts
@@ -0,0 +1,76 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApiBreadService } from '../../core/services/api-bread.service';
+import { Subject, take, takeUntil } from 'rxjs';
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogContent,
+  MatDialogModule,
+} from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-search-result',
+  standalone: true,
+  imports: [MatDialogModule],
+  templateUrl: './search-result.component.html',
+  styleUrl: './search-result.component.scss',
+})
+export class SearchResultComponent implements OnInit {
+  constructor(
+    private route: ActivatedRoute,
+    private apiBreadService: ApiBreadService,
+    public dialog: MatDialog
+  ) {}
+
+  private destroy$ = new Subject();
+  public images: string[] = [];
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params) => {
+      params['sub_breed']
+        ? this.getImagesBySubBreed(params['breed'], params['sub_breed'])
+        : this.getImagesByBreed(params['breed']);
+    });
+  }
+
+  getImagesBySubBreed(breed: string, subbreed: string) {
+    this.apiBreadService
+      .getImagesBySubBreed(breed, subbreed)
+      .pipe(take(1), takeUntil(this.destroy$))
+      .subscribe((res) => {
+        this.images = res;
+      });
+  }
+
+  getImagesByBreed(breed: string) {
+    this.apiBreadService
+      .getImagesByBreed(breed)
+      .pipe(take(1), takeUntil(this.destroy$))
+      .subscribe((res) => {
+        console.log(res, 'res');
+        this.images = res;
+      });
+  }
+
+  openImageModal(imageSrc: string): void {
+    this.dialog.open(ImageModalComponent, {
+      data: imageSrc,
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+    this.destroy$.complete();
+  }
+}
+
+@Component({
+  selector: 'image-dialog',
+  templateUrl: 'image-dialog.html',
+  standalone: true,
+  imports: [MatDialogContent],
+})
+export class ImageModalComponent {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: string) {}
+}

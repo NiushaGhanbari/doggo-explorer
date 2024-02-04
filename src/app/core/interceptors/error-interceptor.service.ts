2790b360b143d7e4a99bd46e18801dd1d7cef31e
Niusha Ghanbari <niusha.gh93@gmail.com>
2024-02-04 15:53:09 +0100

    Add error-interceptor

---
+++ src/app/core/interceptors/error-interceptor.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpResponse,
+  HttpErrorResponse,
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return next.handle(request).pipe(
+      catchError((error: HttpErrorResponse) => {
+        let errorMessage = 'An unknown error occurred';
+        if (error.error instanceof ErrorEvent) {
+          // Client-side errors
+          errorMessage = `Error: ${error.error.message}`;
+        } else {
+          // Server-side errors
+          errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+        }
+        console.error(errorMessage);
+        return throwError(errorMessage);
+      })
+    );
+  }
+}
--- src/app/core/interceptors/index.ts
+++ src/app/core/interceptors/index.ts
@@ -1,8 +1,10 @@
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { ResponseInterceptor } from './response-interceptor.service';
 import { StatusInterceptor } from './status-interceptor.service';
+import { ErrorInterceptor } from './error-interceptor.service';
 
 export const httpInterceptorProviders = [
   { provide: HTTP_INTERCEPTORS, useClass: ResponseInterceptor, multi: true },
   { provide: HTTP_INTERCEPTORS, useClass: StatusInterceptor, multi: true },
+  { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
 ];
